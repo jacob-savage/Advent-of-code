f40c9d91bfe6c618259222e7ecd7fcd4f8bebd2a
Jacob Savage <savagejacob13@gmail.com>
2023-04-16 13:18:00 +1000

    day 6
    partial solution for day 6 and part 2 for day 5 submitted

--- 2015/day5.py
+++ 2015/day5.py
@@ -18,11 +18,11 @@ How many strings are nice?
 '''
 
 def import_file():
-    return open(' 2015/day5.txt').read()
+    return open('day5.txt').readlines()
 
 def filter_string(input_string, allowed_chars):
     filtered_chars = filter(lambda x: x in allowed_chars, input_string)
-    if len(set(filtered_chars)) >= 3:
+    if len(list(filtered_chars)) >= 3:
         return True
     return False
 
@@ -64,4 +64,53 @@ def solution1(input_strings):
 
 
 file = import_file()
+
+# print(solution1(file))
+
+'''
+--- Part Two ---
+Realizing the error of his ways, Santa has switched to a better model of determining whether a string is naughty or nice. None of the old rules apply, as they are all clearly ridiculous.
+
+Now, a nice string is one with all of the following properties:
+
+It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+For example:
+
+qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a letter that repeats with exactly one letter between them (zxz).
+xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between, even though the letters used by each rule overlap.
+uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between them.
+ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair that appears twice.
+'''
+
+import re
+
+def check_line(line):
+    nice = 0
+    for index,_ in enumerate(line):
+        if index == len(line) -2:
+            break
+
+        substring = line[index:index+2]
+        mod_line = line[:index] + line[index+2:]
+
+        print(substring, mod_line)
+
+        result = re.findall(substring, mod_line)
+        if result:
+            nice = 1
+    return nice
+
+def solution2(file):
+    nice = 0
+    for line in file:
+        nice += check_line(line)
+    return nice
+
+
+test = ['uurcxstgmygtbstg']
+
+
+print(solution2(test))
+
 
